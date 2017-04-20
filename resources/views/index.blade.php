a69cd981d0b96e60dece94d3ab1d8f46c6888d8e
John Moore <johnmoore314@gmail.com>
2017-04-19 21:34:50 -0500

    Add bootstrap, implement layouts to give code more DRY feel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,17 @@
 @extends('layout')
 
 @section('content')
-  <h1>Hello, <?php echo $name; ?></h1>
-  <a href="about">About Me</a>
-  <a href="extrapage">Extra</a>
-  <a href="/tasks">List of tasks</a>
+
+<section class="jumbotron text-center">
+  <div class="container">
+    <h1 class="jumbotron-heading">My Laravel Test Blog</h1>
+    <h1>Hello, <?php echo $name; ?></h1>
+    <p>
+      <a href="about" class="btn btn-primary">About Me</a>
+      <a href="extrapage" class="btn btn-primary">Extra</a>
+      <a href="/tasks" class="btn btn-secondary">List of tasks</a>
+    </p>
+  </div>
+</section>
+
 @endsection
